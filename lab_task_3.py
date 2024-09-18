ac3e470f528cf4c5025621c8e610eea76d5100c0
Avani Donga <avanidonga1234@gmail.com>
2024-09-18 09:59:36 -0400

    git add

--- lab_task_3.py
+++ lab_task_3.py
@@ -1,36 +1,3 @@
-# Declare variable
-age = 30
-price = 9.99
-name = "Alice"
-is_student = True 
-
-# Perform arithmetic operations
-sum_value = age + 10
-product_value = price * 2
-
-# Print result
-print("Age:", age)
-print("Price:", price)
-print("Name:", name)
-print("Is student:", is_student)
-print("Sum value:", sum_value)
-print("Product value:", product_value)
-print("")
-print(type(price))
-print(type(age))
-print(type(name))
-print(type(is_student))
-
-
-#Ask to input data
-number = float(input('Enter a number:'))
-
-#Check if the number is even or odd
-if number % 2 == 0:
-    print(number, "is even")
-else:
-    print(number, "is odd")
-
 # Continously ask the user for input until a valid number is provided
 while True:
     user_input = input("Enter a number: ")
---
+++ lab_task__1.py
@@ -0,0 +1,23 @@
+# Declare variable
+age = 30
+price = 9.99
+name = "Alice"
+is_student = True 
+
+# Perform arithmetic operations
+sum_value = age + 10
+product_value = price * 2
+
+
+# Print result
+print("Age:", age)
+print("Price:", price)
+print("Name:", name)
+print("Is student:", is_student)
+print("Sum value:", sum_value)
+print("Product value:", product_value)
+print("")
+print(type(price))
+print(type(age))
+print(type(name))
+print(type(is_student))
