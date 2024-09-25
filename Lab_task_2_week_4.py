d0a043eaadf558fecb05e5ff68135baabc158455
Avani Donga <avanidonga1234@gmail.com>
2024-09-25 12:05:50 -0400

    Lab_task_1_week_4 Lab_task_2_week_4 Lab_task_3_week_4

---
+++ Lab_task_2_week_4.py
@@ -0,0 +1,12 @@
+# Creat a dictionary with key-value pairs
+student = {"name": "Alice", "age": 21, "major": "Computer Science"}
+
+#update a value in the dictionary
+student["age"] = 22
+
+# Access elements by key
+student_name = student["name"]
+
+#print the dictionary
+print("Student:", student)
+print("Student namr:", student_name)
